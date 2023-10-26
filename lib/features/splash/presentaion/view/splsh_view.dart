454ad31a25a28668274aa0a9019f4497f79c8038
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-26 12:18:50 +0300

    add text to splsh view

--- lib/features/splash/presentaion/view/splsh_view.dart
+++ lib/features/splash/presentaion/view/splsh_view.dart
@@ -1,3 +1,6 @@
+import 'package:dalel_app/core/utls/app_strings.dart';
+import 'package:dalel_app/core/utls/text_styles.dart';
+
 import 'package:flutter/material.dart';
 
 class SplshView extends StatelessWidget {
@@ -5,6 +8,12 @@ class SplshView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+          child: Text(
+        AppStrings.appName,
+        style: CustomTextStyles.pacifico400style64,
+      )),
+    );
   }
 }
