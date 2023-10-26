eefb7c51650145419b91e29879876759a9c7b82c
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-26 11:29:28 +0300

    create splsh_view

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,9 @@
+import 'package:dalel_app/features/splash/presentaion/view/splsh_view.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const SplshView(),
+  )
+]);
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dalel_app/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,22 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeView(),
     );
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
