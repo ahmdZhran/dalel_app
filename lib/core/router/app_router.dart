8735395d48560f4590b7470a5daceb0af072ffff
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-26 12:38:05 +0300

    add on bording to router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:dalel_app/core/utls/on_boarding/presentaion/view/on_bording_view.dart';
 import 'package:dalel_app/features/splash/presentaion/view/splsh_view.dart';
 import 'package:go_router/go_router.dart';
 
@@ -5,5 +6,9 @@ final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (context, state) => const SplshView(),
+  ),
+  GoRoute(
+    path: '/onBoarding',
+    builder: (context, state) => const OnBoardingView(),
   )
 ]);
