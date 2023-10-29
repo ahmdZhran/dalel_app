1a9e3b6c054280b8168a0a251d0ab8e116e8fb83
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-29 09:40:44 +0200

    make skip clicable

--- lib/features/on_boarding/presentaion/view/on_bording_view.dart
+++ lib/features/on_boarding/presentaion/view/on_bording_view.dart
@@ -3,6 +3,7 @@ import 'package:dalel_app/core/utls/app_strings.dart';
 import 'package:dalel_app/core/utls/text_styles.dart';
 import 'package:dalel_app/core/widgets/custom_button.dart';
 import 'package:dalel_app/features/on_boarding/data/on_boarding_model.dart';
+import 'package:dalel_app/features/on_boarding/presentaion/widgets/custom_nav_bar.dart';
 import 'package:dalel_app/features/on_boarding/presentaion/widgets/on_boarding_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -25,7 +26,11 @@ class _OnBoardingViewState extends State<OnBoardingView> {
           const SizedBox(
             height: 40,
           ),
-          CustomNavBar(),
+          CustomNavBar(
+            onTap: () {
+              customNavigate(context, 'signUpView');
+            },
+          ),
           OnBoardingWidgetBody(
             controller: _controller,
             onPageChanged: (index) {
--- lib/features/on_boarding/presentaion/widgets/custom_nav_bar.dart
+++ lib/features/on_boarding/presentaion/widgets/custom_nav_bar.dart
@@ -5,16 +5,20 @@ import 'package:flutter/material.dart';
 class CustomNavBar extends StatelessWidget {
   const CustomNavBar({
     super.key,
+    required this.onTap,
   });
-
+  final VoidCallback onTap;
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topRight,
-      child: Text(
-        AppStrings.skip,
-        style: CustomTextStyles.poppins300style16
-            .copyWith(fontWeight: FontWeight.bold),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Text(
+          AppStrings.skip,
+          style: CustomTextStyles.poppins300style16
+              .copyWith(fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
