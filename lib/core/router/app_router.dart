7ab721af490bb8a474763160b9b9901e6f0940e5
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-29 09:25:18 +0200

    add signUpview to app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,6 +2,8 @@ import 'package:dalel_app/features/on_boarding/presentaion/view/on_bording_view.
 import 'package:dalel_app/features/splash/presentaion/view/splsh_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/Auth/presentaion/view/sign_up_view.dart';
+
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(
