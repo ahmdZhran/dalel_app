a8d41fa510a0bfd9ba5c28e234bddd31da4e9d0d
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-29 08:32:20 +0200

    add page controller & convert to statefull widget

--- lib/features/on_boarding/presentaion/view/on_bording_view.dart
+++ lib/features/on_boarding/presentaion/view/on_bording_view.dart
@@ -4,8 +4,15 @@ import 'package:dalel_app/core/widgets/custom_button.dart';
 import 'package:dalel_app/features/on_boarding/presentaion/widgets/on_boarding_widget.dart';
 import 'package:flutter/material.dart';
 
-class OnBoardingView extends StatelessWidget {
+class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
+
+  @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
