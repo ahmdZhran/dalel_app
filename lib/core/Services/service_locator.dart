d19d3ecab9af0a53b6aac3dd07ed0400462bd4bb
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-30 20:49:41 +0200

    rename method of get it

--- lib/core/Services/service_locator.dart
+++ lib/core/Services/service_locator.dart
@@ -2,6 +2,6 @@ import 'package:dalel_app/core/database/cach/cash_helper.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
-void setUp() {
+void setUpServiceLocator() {
   getIt.registerSingleton<CacheHelper>(CacheHelper());
 }
