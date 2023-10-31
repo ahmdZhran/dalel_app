0aecb778a88bd5f3c523b2cb825630dd99215281
Ahmed <ahmedmohameddx80@gmail.com>
2023-10-31 20:51:37 +0200

    add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:dalel_app/core/Services/service_locator.dart';
 import 'package:dalel_app/core/database/cach/cash_helper.dart';
 import 'package:dalel_app/core/router/app_router.dart';
+import 'package:dalel_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setUpServiceLocator();
   await getIt<CacheHelper>().init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Dalel());
 }
 
