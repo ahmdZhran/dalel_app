ca693cf5683e2fbd508b7dd534b355928b8846e5
Ahmed <ahmedmohameddx80@gmail.com>
2023-11-07 06:14:59 +0200

    delete unneccesary import file

--- lib/features/home/data/presentation/views/home_view.dart
+++ lib/features/home/data/presentation/views/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:dalel_app/core/functions/navigator_method.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
