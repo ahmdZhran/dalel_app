6131b038a9137a8a5146afb515e71b6cc02bf95c
Ahmed <ahmedmohameddx80@gmail.com>
2024-01-29 10:48:58 +0200

    refactor to code

--- lib/features/home/data/presentation/widgets/historical_period_item.dart
+++ lib/features/home/data/presentation/widgets/historical_period_item.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:dalel_app/core/utls/app_assets.dart';
 import 'package:dalel_app/core/utls/app_color.dart';
 import 'package:dalel_app/core/utls/text_styles.dart';
-import 'package:flutter/material.dart';
 
 class HistoricalPeriod extends StatelessWidget {
-  const HistoricalPeriod({super.key});
+  const HistoricalPeriod({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +19,7 @@ class HistoricalPeriod extends StatelessWidget {
 }
 
 class HistoricalPeriodItem extends StatelessWidget {
-  const HistoricalPeriodItem({super.key});
+  const HistoricalPeriodItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,35 +30,51 @@ class HistoricalPeriodItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(12),
         boxShadow: [
           BoxShadow(
-              color: AppColors.grey, blurRadius: 7, offset: const Offset(0, 5))
+            color: AppColors.grey,
+            blurRadius: 7,
+            offset: const Offset(0, 5),
+          ),
         ],
         color: Colors.white,
       ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        const SizedBox(),
-        SizedBox(
-          height: 47,
-          width: 62,
-          child: Text(
-            'Ancient Egypt',
-            maxLines: 2,
-            textAlign: TextAlign.center,
-            style: CustomTextStyles.poppins500style18
-                .copyWith(color: AppColors.deepBrown, fontSize: 14),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(),
+          _buildText(),
+          _buildImage(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    return SizedBox(
+      height: 47,
+      width: 62,
+      child: Text(
+        'Ancient Egypt',
+        maxLines: 2,
+        textAlign: TextAlign.center,
+        style: CustomTextStyles.poppins500style18.copyWith(
+          color: AppColors.deepBrown,
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return Container(
+      height: 96,
+      width: 60,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            Assets.imageFrame,
           ),
         ),
-        Container(
-          height: 96,
-          width: 60,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                Assets.imageFrame,
-              ),
-            ),
-          ),
-        )
-      ]),
+      ),
     );
   }
 }
