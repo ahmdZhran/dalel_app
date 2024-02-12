1953a41d0c855f14abda98491ca5395ccd635d19
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-12 05:05:36 +0200

    get data from firebase

--- lib/features/home/data/presentation/views/home_sections/historical_period_section.dart
+++ lib/features/home/data/presentation/views/home_sections/historical_period_section.dart
@@ -1,8 +1,9 @@
-import '../../../../../../core/utls/app_strings.dart';
-import '../../../../../../core/widgets/custom_header_text.dart';
-import '../../widgets/historical_period_item.dart';
+import 'package:dalel_app/core/widgets/custom_header_text.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../../../../../core/utls/app_strings.dart';
+import '../../widgets/historical_period_item.dart';
 
 class HistoricalPeriodSection extends StatelessWidget {
   const HistoricalPeriodSection({super.key});
@@ -12,31 +13,16 @@ class HistoricalPeriodSection extends StatelessWidget {
     return FutureBuilder<QuerySnapshot>(
       future: FirebaseFirestore.instance.collection('historical_period').get(),
       builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Text("Something went wrong");
-        }
-
-        if (snapshot.hasData && !snapshot.data!.docs[0].exists) {
-          return const Text("Document does not exist");
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-              snapshot.data!.docs[0].data() as Map<String, dynamic>;
-          return Text(data['name']);
-        }
-
-        return const Text("loading");
-        // return  Column(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     const CustomHeaderText(text: AppStrings.historicalPeriods),
-        //     const SizedBox(height: 20),
-        //     Text(snapshot.data!.docs[0]['name']),
-        //     const HistoricalPeriod(),
-        //     const SizedBox(height: 40),
-        //   ],
-        // );
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CustomHeaderText(text: AppStrings.historicalPeriods),
+            const SizedBox(height: 20),
+            Text(snapshot.data!.docs[0]['name']),
+            const HistoricalPeriod(),
+            const SizedBox(height: 40),
+          ],
+        );
       },
     );
   }
--- lib/features/home/data/presentation/widgets/historical_period_item.dart
+++ lib/features/home/data/presentation/widgets/historical_period_item.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dalel_app/features/home/data/presentation/widgets/historical_period_card.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +7,45 @@ class HistoricalPeriod extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        HistoricalPeriodItem(),
-        HistoricalPeriodItem(),
-      ],
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance.collection('historical_period').get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.docs[0].exists) {
+          return const Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return SizedBox(
+            height: 96,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const HistoricalPeriodItem();
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 20);
+              },
+              itemCount: 2,
+            ),
+          );
+        }
+
+        return const Text("loading");
+        // return  Column(
+        //   crossAxisAlignment: CrossAxisAlignment.start,
+        //   children: [
+        //     const CustomHeaderText(text: AppStrings.historicalPeriods),
+        //     const SizedBox(height: 20),
+        //     Text(snapshot.data!.docs[0]['name']),
+        //     const HistoricalPeriod(),
+        //     const SizedBox(height: 40),
+        //   ],
+        // );
+      },
     );
   }
 }
