b009f23e42f8e74baa35513bb469e4d4e291ac31
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-12 05:12:47 +0200

    refactor our code

--- lib/features/home/data/presentation/views/home_sections/historical_period_section.dart
+++ lib/features/home/data/presentation/views/home_sections/historical_period_section.dart
@@ -1,6 +1,5 @@
 import 'package:dalel_app/core/widgets/custom_header_text.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../../../../../core/utls/app_strings.dart';
 import '../../widgets/historical_period_item.dart';
@@ -10,20 +9,14 @@ class HistoricalPeriodSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-      future: FirebaseFirestore.instance.collection('historical_period').get(),
-      builder: (context, snapshot) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const CustomHeaderText(text: AppStrings.historicalPeriods),
-            const SizedBox(height: 20),
-            Text(snapshot.data!.docs[0]['name']),
-            const HistoricalPeriod(),
-            const SizedBox(height: 40),
-          ],
-        );
-      },
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomHeaderText(text: AppStrings.historicalPeriods),
+        SizedBox(height: 20),
+        HistoricalPeriod(),
+        SizedBox(height: 40),
+      ],
     );
   }
 }
