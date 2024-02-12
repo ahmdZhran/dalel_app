72ef04cc4c0e25da9f4049e93ae25870350a09e2
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-12 08:10:50 +0200

    add home states

--- lib/features/home/data/presentation/cubit/home_cubit.dart
+++ lib/features/home/data/presentation/cubit/home_cubit.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dalel_app/features/home/data/models/historical_period_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -5,4 +7,16 @@ part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
+  List<HistoricalPeriodsModel> historicalPeriodModels = [];
+  getHistoricalPeriods() async {
+    await FirebaseFirestore.instance.collection('historical_period').get().then(
+          (value) => value.docs.forEach(
+            (element) {
+              historicalPeriodModels.add(
+                HistoricalPeriodsModel.fromJason(element.data()),
+              );
+            },
+          ),
+        );
+  }
 }
--- lib/features/home/data/presentation/cubit/home_state.dart
+++ lib/features/home/data/presentation/cubit/home_state.dart
@@ -4,3 +4,13 @@ part of 'home_cubit.dart';
 abstract class HomeState {}
 
 class HomeInitial extends HomeState {}
+
+class HomeLoading extends HomeState {}
+
+class HomeSuccess extends HomeState {}
+
+class HomeFailer extends HomeState {
+  final String errMessage;
+
+  HomeFailer({required this.errMessage});
+}
