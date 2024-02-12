faf9c133fc892c3dbe5a80d48721c8b7659308b2
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-12 08:35:58 +0200

    home cubit done

--- lib/features/home/data/presentation/cubit/home_cubit.dart
+++ lib/features/home/data/presentation/cubit/home_cubit.dart
@@ -7,24 +7,35 @@ part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
+
+  // List to store historical period models
   List<HistoricalPeriodsModel> historicalPeriodModels = [];
+
+  // Method to fetch historical periods from Firestore
   getHistoricalPeriods() async {
     try {
+      // Emit loading state to notify listeners
       emit(GetHistoricalPeriodLoading());
+
+      // Fetch historical periods from Firestore
       await FirebaseFirestore.instance
           .collection('historical_period')
           .get()
           .then(
             (value) => value.docs.forEach(
               (element) {
+                // Convert Firestore document data into HistoricalPeriodsModel
                 historicalPeriodModels.add(
-                  HistoricalPeriodsModel.fromJason(element.data()),
+                  HistoricalPeriodsModel.fromJson(element.data()),
                 );
               },
             ),
           );
+
+      // Emit success state after successfully fetching historical periods
       emit(GetHistoricalPeriodSuccess());
     } on Exception catch (e) {
+      // Emit failure state if an exception occurs during fetching
       emit(GetHistoricalPeriodFailer(errMessage: e.toString()));
     }
   }
--- lib/features/home/data/presentation/widgets/home_navigatoin_bar.dart
+++ lib/features/home/data/presentation/widgets/home_navigatoin_bar.dart
@@ -38,7 +38,7 @@ class HomeNavBarWidget extends StatelessWidget {
 List<Widget> _buildScreens() {
   return [
     BlocProvider(
-      create: (context) => HomeCubit(),
+      create: (context) => HomeCubit()..getHistoricalPeriods(),
       child: const HomeView(),
     ),
     const CartView(),
