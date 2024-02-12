093aaf09ec5f49106b0bfb4e09d31205ac35f3dd
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-12 07:58:30 +0200

    add Bloc consumer on Future builder

--- lib/features/home/data/presentation/widgets/historical_period_item.dart
+++ lib/features/home/data/presentation/widgets/historical_period_item.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dalel_app/features/home/data/models/historical_period_model.dart';
+import 'package:dalel_app/features/home/data/presentation/cubit/home_cubit.dart';
 import 'package:dalel_app/features/home/data/presentation/widgets/historical_period_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../../core/widgets/custom_shimmer_category.dart';
 
@@ -10,41 +12,49 @@ class HistoricalPeriod extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-      future: FirebaseFirestore.instance.collection('historical_period').get(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return const Text("Something went wrong");
-        }
+    return BlocConsumer<HomeCubit, HomeState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return FutureBuilder<QuerySnapshot>(
+          future:
+              FirebaseFirestore.instance.collection('historical_period').get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text("Something went wrong");
+            }
 
-        if (snapshot.hasData && !snapshot.data!.docs[0].exists) {
-          return const Text("Document does not exist");
-        }
+            if (snapshot.hasData && !snapshot.data!.docs[0].exists) {
+              return const Text("Document does not exist");
+            }
 
-        if (snapshot.connectionState == ConnectionState.done) {
-          List<HistoricalPeriodsModel> historicalPeriods = [];
-          for (int i = 0; i < snapshot.data!.docs.length; i++) {
-            historicalPeriods
-                .add(HistoricalPeriodsModel.fromJason(snapshot.data!.docs[i]));
-          }
-          return SizedBox(
-            height: 96,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return HistoricalPeriodItem(
-                  historicalPeriodsModel: historicalPeriods[index],
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(width: 20);
-              },
-              itemCount: snapshot.data!.docs.length,
-            ),
-          );
-        }
+            if (snapshot.connectionState == ConnectionState.done) {
+              List<HistoricalPeriodsModel> historicalPeriods = [];
+              for (int i = 0; i < snapshot.data!.docs.length; i++) {
+                historicalPeriods.add(
+                    HistoricalPeriodsModel.fromJason(snapshot.data!.docs[i]));
+              }
+              return SizedBox(
+                height: 96,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return HistoricalPeriodItem(
+                      historicalPeriodsModel: historicalPeriods[index],
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(width: 20);
+                  },
+                  itemCount: snapshot.data!.docs.length,
+                ),
+              );
+            }
 
-        return const CustomShimmerCategory();
+            return const CustomShimmerCategory();
+          },
+        );
       },
     );
   }
