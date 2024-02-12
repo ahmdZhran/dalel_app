bcdeffdb8c6e714f9bafb33df8b94f45d595d21d
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-12 04:52:24 +0200

    refactor our code

--- lib/features/home/data/presentation/views/home_sections/historical_period_section.dart
+++ lib/features/home/data/presentation/views/home_sections/historical_period_section.dart
@@ -2,20 +2,42 @@ import '../../../../../../core/utls/app_strings.dart';
 import '../../../../../../core/widgets/custom_header_text.dart';
 import '../../widgets/historical_period_item.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HistoricalPeriodSection extends StatelessWidget {
   const HistoricalPeriodSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomHeaderText(text: AppStrings.historicalPeriods),
-        SizedBox(height: 20),
-        HistoricalPeriod(),
-        SizedBox(height: 40),
-      ],
+    return FutureBuilder<QuerySnapshot>(
+      future: FirebaseFirestore.instance.collection('historical_period').get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.docs[0].exists) {
+          return const Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.docs[0].data() as Map<String, dynamic>;
+          return Text(data['name']);
+        }
+
+        return const Text("loading");
+        // return  Column(
+        //   crossAxisAlignment: CrossAxisAlignment.start,
+        //   children: [
+        //     const CustomHeaderText(text: AppStrings.historicalPeriods),
+        //     const SizedBox(height: 20),
+        //     Text(snapshot.data!.docs[0]['name']),
+        //     const HistoricalPeriod(),
+        //     const SizedBox(height: 40),
+        //   ],
+        // );
+      },
     );
   }
 }
--- lib/features/home/data/presentation/widgets/historical_period_item.dart
+++ lib/features/home/data/presentation/widgets/historical_period_item.dart
@@ -1,7 +1,5 @@
+import 'package:dalel_app/features/home/data/presentation/widgets/historical_period_card.dart';
 import 'package:flutter/material.dart';
-import '../../../../../core/utls/app_assets.dart';
-import '../../../../../core/utls/app_color.dart';
-import '../../../../../core/utls/text_styles.dart';
 
 class HistoricalPeriod extends StatelessWidget {
   const HistoricalPeriod({Key? key}) : super(key: key);
@@ -14,67 +12,6 @@ class HistoricalPeriod extends StatelessWidget {
         HistoricalPeriodItem(),
         HistoricalPeriodItem(),
       ],
-    );
-  }
-}
-
-class HistoricalPeriodItem extends StatelessWidget {
-  const HistoricalPeriodItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      width: 180,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.grey,
-            blurRadius: 7,
-            offset: const Offset(0, 5),
-          ),
-        ],
-        color: Colors.white,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(),
-          _buildText(),
-          _buildImage(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildText() {
-    return SizedBox(
-      height: 47,
-      width: 62,
-      child: Text(
-        'Ancient Egypt',
-        maxLines: 2,
-        textAlign: TextAlign.center,
-        style: CustomTextStyles.poppins500style18.copyWith(
-          color: AppColors.deepBrown,
-          fontSize: 14,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildImage() {
-    return Container(
-      height: 96,
-      width: 60,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            Assets.imageFrame,
-          ),
-        ),
-      ),
     );
   }
 }
