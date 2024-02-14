a003e9af51967e2fea078a496d2950a9501cba70
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-14 10:12:26 +0200

    create recommendation section

--- lib/features/home/data/presentation/views/historical_period_details.dart
+++ lib/features/home/data/presentation/views/historical_period_details.dart
@@ -1,6 +1,9 @@
 import 'package:dalel_app/features/home/data/models/historical_period_model.dart';
 import 'package:dalel_app/features/home/data/presentation/widgets/home_sections/app_bar_section.dart';
+import 'package:dalel_app/features/home/data/presentation/widgets/list_of_historical_character.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utls/app_strings.dart';
+import '../../../../../core/widgets/custom_header_text.dart';
 import '../widgets/period_details_section.dart';
 import '../widgets/period_wars_section.dart';
 
@@ -29,10 +32,30 @@ class HistoricalPeriodDetailsView extends StatelessWidget {
               child: HistoricalWarSection(
                 warsList: historicalPeriodModel.wars,
               ),
+            ),
+            const SliverToBoxAdapter(
+              child: RecommendationSectionView(),
             )
           ],
         ),
       ),
     ));
+  }
+}
+
+class RecommendationSectionView extends StatelessWidget {
+  const RecommendationSectionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 40),
+        CustomHeaderText(text: AppStrings.recommendations),
+        SizedBox(height: 16),
+        ListOfHistoricalCharacter()
+      ],
+    );
   }
 }
