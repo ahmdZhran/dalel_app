79ac2f5e8a345e33173b87930876d8b819c18100
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-14 08:44:45 +0200

    start creating historical period details view

---
+++ lib/features/home/data/presentation/views/historical_period_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HistoricalPeriodDetailsView extends StatelessWidget {
+  const HistoricalPeriodDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('data')),
+    );
+  }
+}
