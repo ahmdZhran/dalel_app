82752a0ea487bdebe7a73ca9e426f6414b7eccb2
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-14 09:31:42 +0200

    move home section folder

--- lib/features/home/data/presentation/views/historical_period_details.dart
+++ lib/features/home/data/presentation/views/historical_period_details.dart
@@ -1,8 +1,10 @@
+import 'package:dalel_app/core/utls/app_assets.dart';
 import 'package:dalel_app/core/utls/app_strings.dart';
 import 'package:dalel_app/core/widgets/custom_header_text.dart';
 import 'package:dalel_app/features/home/data/models/historical_period_model.dart';
-import 'package:dalel_app/features/home/data/presentation/views/home_sections/app_bar_section.dart';
+import 'package:dalel_app/features/home/data/presentation/widgets/home_sections/app_bar_section.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HistoricalPeriodDetailsView extends StatelessWidget {
   const HistoricalPeriodDetailsView(
@@ -10,12 +12,16 @@ class HistoricalPeriodDetailsView extends StatelessWidget {
   final HistoricalPeriodsModel historicalPeriodModel;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: SafeArea(
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(child: HomeAppBarSection()),
-          SliverToBoxAdapter(child: SizedBox(height: 7)),
+          const SliverToBoxAdapter(child: HomeAppBarSection()),
+          const SliverToBoxAdapter(child: SizedBox(height: 7)),
+          SliverToBoxAdapter(
+              child: PeriodDetailsSection(
+            periodNam: historicalPeriodModel.name,
+          )),
         ],
       ),
     ));
@@ -30,7 +36,11 @@ class PeriodDetailsSection extends StatelessWidget {
     return Column(
       children: [
         Row(
-          children: [CustomHeaderText(text: "${AppStrings.about} $periodNam")],
+          children: [
+            CustomHeaderText(text: "${AppStrings.about} $periodNam"),
+            const SizedBox(width: 7),
+            SvgPicture.asset(Assets.lifeKey)
+          ],
         )
       ],
     );
--- lib/features/home/data/presentation/widgets/home_sections/historical_character_section.dart
+++ lib/features/home/data/presentation/widgets/home_sections/historical_character_section.dart
@@ -1,6 +1,6 @@
 import '../../../../../../core/utls/app_strings.dart';
 import '../../../../../../core/widgets/custom_header_text.dart';
-import '../../widgets/list_of_historical_character.dart';
+import '../list_of_historical_character.dart';
 import 'package:flutter/material.dart';
 
 class HistoricalCharacterSection extends StatelessWidget {
