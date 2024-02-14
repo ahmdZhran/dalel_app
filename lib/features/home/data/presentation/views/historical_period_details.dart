832473bb176645d8cac8ff5030daf3942eebab44
Ahmed <ahmedmohameddx80@gmail.com>
2024-02-14 09:33:57 +0200

    import Period details section in Historical period details view

--- lib/features/home/data/presentation/views/historical_period_details.dart
+++ lib/features/home/data/presentation/views/historical_period_details.dart
@@ -1,10 +1,7 @@
-import 'package:dalel_app/core/utls/app_assets.dart';
-import 'package:dalel_app/core/utls/app_strings.dart';
-import 'package:dalel_app/core/widgets/custom_header_text.dart';
 import 'package:dalel_app/features/home/data/models/historical_period_model.dart';
 import 'package:dalel_app/features/home/data/presentation/widgets/home_sections/app_bar_section.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import '../widgets/period_details_section.dart';
 
 class HistoricalPeriodDetailsView extends StatelessWidget {
   const HistoricalPeriodDetailsView(
